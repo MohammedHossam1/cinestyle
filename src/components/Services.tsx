3028c99023ff384a717bb67b95851386173e1954
mohammedhossam1 <mohammedhossam199998@gmail.com>
2025-06-25 02:40:26 +0300

    Handled services adding from supabase

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,10 +1,25 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import ServiceCard from "../components/ServiceCard";
-import { ServicesData } from "../data/Index";
+import { services } from "../lib/subaMethods";
+import { IServices } from "../types/Index";
 export default function Services() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [servicesData, setServicesData] = useState<IServices[]>([]);
+const isAr= i18n.language === "ar";
+  useEffect(() => {
+    const getServices = async () => {
+      try {
+        const data = await services();
+        setServicesData(data);
+      } catch (error) {
+        console.error('Error fetching services:', error);
+      }
+    }
 
+    getServices();
+  }, []);
 
 
   return (
@@ -21,20 +36,19 @@ export default function Services() {
         </motion.div>
 
         <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {ServicesData.map((service, index) => (
-            <motion.div 
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ delay: index * 0.1 }}
-            key={index}>
+          {servicesData.map((service, index) => (
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ delay: index * 0.1 }}
+              key={index}>
               <ServiceCard
-                title={t(`${service.title}.title`)}
-                description={t(`${service.title}.description`)}
-                iconBgColor={service.iconBgColor}
+                title={isAr ? service.titleAr : service.titleEn}
+                description={ isAr ? service.descAr : service.descEn}
                 className="hover:scale-105 transition-transform duration-300"
                 style={{
-                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9)), url(${service.bgImage})`,
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9)), url(${service.image_url})`,
                   backgroundSize: 'cover',
                   backgroundPosition: 'center'
                 }}
